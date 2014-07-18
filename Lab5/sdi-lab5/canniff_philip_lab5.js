e9ed0408be848e07b5478c2b27af9babae7110fd
Pcanniff <philipcanniff@Philips-MacBook-Pro.local>
2014-07-17 21:53:23 -0400

    Lab 5
    Subtraction commit.

--- Lab5/sdi-lab5/canniff_philip_lab5.js
+++ Lab5/sdi-lab5/canniff_philip_lab5.js
@@ -10,6 +10,18 @@ var addition = function(num1, num2) {
 		
 		solution = (num1 + num2);
 		return solution;
+} else {
+
+}
+};
+
+var subtract = function(num1, num2) {
+	
+	var solution;
+	if (symbol === "-") {
+		
+		solution = (num1 - num2);
+		return solution;
 }
 };
 
@@ -17,9 +29,12 @@ numOne = parseFloat(prompt("What is your first number?"));
 numTwo = parseFloat(prompt("what is your second number?"));
 symbol = prompt("What form of arithmetic are you using? EX: +, -, *, or /");
 
+var evaluationsub = subtract(numOne, numTwo);
 var evaluation = addition(numOne, numTwo);
+
+if (symbol === "+") {
 console.log(evaluation);
-
-
-
-
+}
+if (symbol === "-") {
+console.log(evaluationsub); 
+}
