96fc571858c016f71999437e2f65e8605bb30204
Pcanniff <philipcanniff@Philips-MacBook-Pro.local>
2014-07-17 22:29:38 -0400

    Lab 5
    Multiplication commit.

--- Lab5/sdi-lab5/canniff_philip_lab5.js
+++ Lab5/sdi-lab5/canniff_philip_lab5.js
@@ -28,13 +28,13 @@ var multiply = function(num1, num2) {
 		return solution;
 };
 
-var divide = function(num1, num2) {
-	var solution; 
-		
-		solution = (num1 / num2);
-		return solution;
-
-};
+// var divide = function(num1, num2) {
+	// var solution; 
+// 		
+		// solution = (num1 / num2);
+		// return solution;
+// 
+// };
 
 numOne = parseFloat(prompt("What is your first number?"));
 numTwo = parseFloat(prompt("what is your second number?"));
@@ -43,7 +43,7 @@ symbol = prompt("What form of arithmetic are you using? EX: +, -, *, or /");
 var evaluationsub = subtract(numOne, numTwo);
 var evaluation = addition(numOne, numTwo);
 var evaluationmulti = multiply(numOne, numTwo);
-var evaluationdivide = divide(numOne, numTwo);
+// var evaluationdivide = divide(numOne, numTwo);
 
 if (symbol === "+") {
 console.log("The sum of", numOne, "and", numTwo, "is equal to", evaluation + ".");
@@ -54,11 +54,11 @@ console.log("The difference of", numOne, "and", numTwo, "is equal to", evaluatio
 if (symbol === "*") {
 console.log("The product of", numOne, "and", numTwo, "is equal to", evaluationmulti + ".");
 }
-if (symbol === "/") {
-		if (numTwo === 0) {
-		console.log("Can't divide by 0.");
-} else {
-	console.log("The quotient of", numOne, "and", numTwo, "is equal to", evaluationdivide + ".");
-}
-} 
+// if (symbol === "/") {
+		// if (numTwo === 0) {
+		// console.log("Can't divide by 0.");
+// } else {
+	// console.log("The quotient of", numOne, "and", numTwo, "is equal to", evaluationdivide + ".");
+// }
+// } 
 
