e5efc148b0f9100dcbf025fa432d3d77794052ed
Pcanniff <philipcanniff@Philips-MacBook-Pro.local>
2014-08-05 16:21:06 -0400

    Practical Exam
    Practical

---
+++ sdi-practical/Canniff_Philip_PracticalExam.js
@@ -0,0 +1,50 @@
+alert("JavaScript works!");
+
+//	Philip A. Canniff
+//	SDI 1407
+//	Lee Lewis
+//	Prctical Exam
+
+//Global Variables
+// var testString = "703-4a9-0972";
+// 
+// //Function
+// var examFunction = function(argument) {
+	// //Local Variables
+	// var varifyProp = false;
+	// var newString = "";
+	// var firstDash = argument.indexOf("-");
+	// var secondDash = argument.lastIndexOf("-");
+	// var stringLength = argument.length;
+	// //Conditionals
+	// if (firstDash === 3 && secondDash === 7 && stringLength === 12) {
+		// varifyProp = true;
+		// var newString = argument.replace("-", "");
+		// var newString = newString.replace("-", "");
+		// var newString = parseInt(newString);
+		// var newString = newString.toString();
+	// }else {
+		// varifyProp = false;
+	// }
+	// if (newString.length === 10) {
+		// varifyProp = true;
+	// } else {
+		// varifyprop = false;
+	// }
+	// return varifyProp;
+// };
+// 
+// //Outputs
+// catcher = examFunction(testString);
+// if (catcher === true) {
+	// console.log("It is " + catcher + " that this is a valid phone number! We'll call you at your earliest conveniece!");
+// } else {
+	// console.log("It is " + catcher + " that this is a valid phone number! Please enter numbers only and do not forget dashes!");
+// }
+
+
+
+
+
+
+
